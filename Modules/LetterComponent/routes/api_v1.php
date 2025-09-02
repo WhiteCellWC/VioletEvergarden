eaeb292778224abcae5ad36b624f2ef9d38d1baf
ToeOoWaiYan <toeoowaiyan4252@gmail.com>
2025-09-02 15:27:32 +0630

    FragranceType api route added

--- Modules/LetterComponent/routes/api_v1.php
+++ Modules/LetterComponent/routes/api_v1.php
@@ -1,4 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\LetterComponent\Http\Controller\Api\V1\FragranceTypeApiController;
 
+Route::apiResource('fragrance-types', FragranceTypeApiController::class);
